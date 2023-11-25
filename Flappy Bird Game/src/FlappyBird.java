86640887aac32df6a6c1f29ee04d62d0269c8cd4
Shazaib Dawood <shazaibdawood@gmail.com>
2023-11-25 13:44:55 -0600

    removing comment

--- Flappy Bird Game/src/FlappyBird.java
+++ Flappy Bird Game/src/FlappyBird.java
@@ -10,7 +10,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Random;
-//njnjnj
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
